e43520772bfb8df59bc6202df2bab8f104d6b788
josopondo <josephineopondo5@gmail.com>
2025-01-23 11:38:39 +0300

    feat: initialize WgetApp state with thread-safe structures

---
+++ wgetApp/dataModels.go
@@ -0,0 +1,48 @@
+package wgetApp
+
+import "sync"
+
+// UrlArgs struct with exported fields (Uppercase names)
+type UrlArgs struct {
+	url              string
+	file             string
+	rateLimit        string
+	path             string
+	sourceFile       string
+	workInBackground bool
+	mirroring        bool
+	rejectFlag       string
+	excludeFlag      string
+	convertLinksFlag bool
+}
+
+// ProcessedURLs is a thread-safe structure that holds a collection of URLs
+type ProcessedURLs struct {
+	sync.Mutex
+	urls map[string]bool
+}
+
+// WgetApp encapsulates global variables and synchronization primitives
+type WgetApp struct {
+	urlArgs        UrlArgs
+	processedURLs  ProcessedURLs
+	visitedPages   map[string]bool
+	visitedAssets  map[string]bool
+	muPages        sync.Mutex
+	muAssets       sync.Mutex
+	semaphore      chan struct{}
+	count          int
+	tempConfigFile string
+}
+
+// newWgetState initializes and returns a new instance of WgetApp.
+func newWgetState() *WgetApp {
+	return &WgetApp{
+		visitedPages:  make(map[string]bool),
+		visitedAssets: make(map[string]bool),
+		processedURLs: ProcessedURLs{
+			urls: make(map[string]bool),
+		},
+		tempConfigFile: "progress_config.txt",
+	}
+}
