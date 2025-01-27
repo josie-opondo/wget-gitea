0ac8975bb71e0522b0b022b8997a0b66cd66f2e5
josopondo <josephineopondo5@gmail.com>
2025-01-27 15:44:20 +0300

    feat: Integrate mirror functionality into taskManager
    - Added integration of the `mirror` method within `taskManager`.
    - Mirrors the website specified by the URL arguments if the mirroring flag is set.
    - Returns any errors encountered during the mirroring process.

--- wgetApp/taskManager.go
+++ wgetApp/taskManager.go
@@ -5,5 +5,14 @@ func (app *WgetApp) taskManager(err error) error {
 		return err
 	}
 
+	// Mirror website handling
+	if app.urlArgs.mirroring {
+		err := app.mirror(app.urlArgs.url, app.urlArgs.rejectFlag, app.urlArgs.excludeFlag, app.urlArgs.convertLinksFlag)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
 	return nil
 }
