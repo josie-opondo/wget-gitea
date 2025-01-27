597a71bc50313f5d707045d772707295e879fa12
josopondo <josephineopondo5@gmail.com>
2025-01-27 14:53:55 +0300

    feat: implement `IsRejected` function to filter URLs based on file extensions
    - Added the `IsRejected` function that checks whether a URL should be rejected based on its file extension.
    - This function compares the URL's file type against a comma-separated list of rejected extensions provided by the `rejectTypes` parameter.
    - It helps in filtering URLs during the download and mirroring process by rejecting unwanted file types.

--- wgetUtils/helpers.go
+++ wgetUtils/helpers.go
@@ -3,6 +3,7 @@ package wgetutils
 import (
 	"fmt"
 	"net/url"
+	"strings"
 )
 
 // ValidateURL checks if the given link is a valid URL.
@@ -21,4 +22,19 @@ func ExtractDomain(urls string) (string, error) {
 		return "", err
 	}
 	return u.Hostname(), nil
+}
+
+// IsRejected checks if the URL ends with any of the rejected file types based on the provided rejectTypes string.
+func IsRejected(url, rejectTypes string) bool {
+	if rejectTypes == "" {
+		return false
+	}
+
+	rejectedTypes := strings.Split(rejectTypes, ",")
+	for _, ext := range rejectedTypes {
+		if strings.HasSuffix(url, ext) {
+			return true
+		}
+	}
+	return false
 }
