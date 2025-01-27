0b426de601d482aa719babae942333f62f8ba308
josopondo <josephineopondo5@gmail.com>
2025-01-27 14:22:05 +0300

    feat: add `mirror` function skeleton
    - Defined the `mirror` function that will handle page mirroring.
    - Set up function parameters (`url`, `rejectTypes`, `rejectPaths`, `convertLink`) for future logic.

---
+++ wgetApp/mirror.go
@@ -0,0 +1,8 @@
+package wgetApp
+
+// mirror handles the mirroring of a webpage, downloading
+// the page and recursively visits linked pages, downloading assets
+// as needed.
+func (app *WgetApp) mirror(url, rejectTypes, rejectPaths string, convertLink bool) error {
+	return nil
+}
