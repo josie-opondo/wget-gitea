e978470e725a7e95fbb2a757819b527bfa1d8ada
josopondo <josephineopondo5@gmail.com>
2025-01-27 15:12:39 +0300

    feat: add `getLocalPath` function to generate local file paths from URLs
    - Converts absolute URLs to local file system paths.
    - Handles HTTP(S) URLs and relative paths appropriately.
    - Ensures proper path formatting for resource storage.

--- wgetUtils/convertLink.go
+++ wgetUtils/convertLink.go
@@ -1,9 +1,26 @@
 package wgetutils
 
 import (
+	"net/url"
+	"path"
 	"regexp"
 	"strings"
 )
+
+// getLocalPath converts a given URL into a local file system path.
+// It handles absolute HTTP(S) URLs, protocol-relative URLs, and root-relative paths.
+func getLocalPath(originalURL string) string {
+	if strings.HasPrefix(originalURL, "http") || strings.HasPrefix(originalURL, "//") {
+		parsedURL, err := url.Parse(originalURL)
+		if err != nil {
+			return originalURL
+		}
+		return path.Join(parsedURL.Host, parsedURL.Path)
+	} else if strings.HasPrefix(originalURL, "/") {
+		return path.Join(".", originalURL)
+	}
+	return originalURL
+}
 
 // removeHTTP removes the http:// or https:// prefix from the URL.
 func removeHTTP(url string) string {
