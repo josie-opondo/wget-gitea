6d37a521a2b2911e0e777026b6e597b3e2243d8b
hanapiko <apikojuma94@gmail.com>
2025-01-28 10:08:30 +0300

    feat: add HTTP status check, response info, and output file naming

--- wgetApp/single_downloader.go
+++ wgetApp/single_downloader.go
@@ -2,7 +2,10 @@ package wgetApp
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	wgetutils "wget/wgetUtils"
@@ -15,7 +18,7 @@ func (app *WgetApp) singleDownloader(file, url, limit, directory string) error {
 	}
 	fileURL := url
 	startTime := time.Now()
-	// toDisplay, err := wgetutils.LoadShowProgressState(app.tempConfigFile)
+	toDisplay, err := wgetutils.LoadShowProgressState(app.tempConfigFile)
 	if err != nil {
 		return err
 	}
@@ -26,6 +29,23 @@ func (app *WgetApp) singleDownloader(file, url, limit, directory string) error {
 		return fmt.Errorf("error downloading file:\nserver misbehaving")
 	}
 	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("error: status %s\nurl: [%s]", resp.Status, url)
+	}
+	fmt.Printf("sending request, awaiting response... status %s\n", resp.Status)
+
+	contentLength := resp.ContentLength
+	fmt.Printf("content size: %d bytes [~%.2fMB]\n", contentLength, float64(contentLength)/1000000)
+
+	// Set the output file name
+	var outputFile string
+	if file == "" {
+		urlParts := strings.Split(fileURL, "/")
+		file = urlParts[len(urlParts)-1]
+		outputFile = filepath.Join(path, file)
+	} else {
+		outputFile = filepath.Join(path, file)
+	}
 
 	if path != "" {
 		err = os.MkdirAll(path, 0o755)
