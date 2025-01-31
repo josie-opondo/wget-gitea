69d15e4d85bc17d269af50e187ae70f7e29feedb
josopondo <josephineopondo5@gmail.com>
2025-01-31 09:42:41 +0300

    ref: remove async downloader - This is duplicate code with issues. It badly does the same thing that singledowloader does better.

--- wgetApp/multi_downloader.go
+++ wgetApp/multi_downloader.go
@@ -3,14 +3,9 @@ package wgetApp
 import (
 	"bufio"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
-	"path/filepath"
 	"strings"
 	"sync"
-
-	wgetutils "wget/wgetUtils"
 )
 
 /*
@@ -37,6 +32,7 @@ func (app *WgetApp) downloadMultipleFiles(filePath, outputFile, limit, directory
 
 	var wg sync.WaitGroup
 	scanner := bufio.NewScanner(file)
+
 	for scanner.Scan() {
 		url := strings.TrimSpace(scanner.Text())
 
@@ -46,7 +42,7 @@ func (app *WgetApp) downloadMultipleFiles(filePath, outputFile, limit, directory
 		wg.Add(1)
 		go func(url string) error {
 			defer wg.Done()
-			err := app.asyncDownload(outputFile, url, limit, directory)
+			err := app.singleDownloader(outputFile, url, limit, directory)
 			if err != nil {
 				return err
 			}
@@ -54,93 +50,6 @@ func (app *WgetApp) downloadMultipleFiles(filePath, outputFile, limit, directory
 		}(url)
 	}
 	wg.Wait()
-
-	return nil
-}
-
-/*
-asyncDownload
-*parameters*
-- outputFileName: The name of the file where the downloaded content will be saved. If empty, the filename is derived from the URL.
-- url: The URL of the file to be downloaded.
-- limit: The download speed limit (if applicable).
-- directory: The directory where the file should be saved.
-
-*functionality*
-- Expands the directory path using `wgetutils.ExpandPath`.
-- Sends an HTTP request to fetch the file.
-- Validates the response status code to ensure a successful request.
-- Determines the output file name, either from the provided `outputFileName` or derived from the URL.
-- Creates the required directory structure if it does not exist.
-- Opens a new file for writing the downloaded content.
-- Reads the response body in chunks and writes to the file, applying a rate limit if specified.
-- Displays download progress and prints a success message upon completion.
-*/
-func (app *WgetApp) asyncDownload(outputFileName, url, limit, directory string) error {
-	path, err := wgetutils.ExpandPath(directory)
-	if err != nil {
-		return err
-	}
-
-	resp, err := wgetutils.HttpRequest(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("error: status %s url:\n[%s]", resp.Status, url)
-	}
-
-	if outputFileName == "" {
-		urlParts := strings.Split(url, "/")
-		fileName := urlParts[len(urlParts)-1]
-		outputFileName = filepath.Join(path, fileName)
-	} else {
-		outputFileName = filepath.Join(path, outputFileName)
-	}
-
-	if path != "" {
-		err = os.MkdirAll(path, 0o755)
-		if err != nil {
-			return fmt.Errorf("error creating directory:\n%v", err)
-		}
-	}
-
-	var out *os.File
-	out, err = os.Create(outputFileName)
-	if err != nil {
-		return fmt.Errorf("error creating file:\n%v", err)
-	}
-	defer out.Close()
-
-	var reader io.Reader = resp.Body
-	if limit != "" {
-		reader = wgetutils.NewRateLimitedReader(resp.Body, limit)
-	}
-
-	buffer := make([]byte, 32*1024)
-	fmt.Printf("Downloading.... [%s]\n", url)
-	var downloaded int64
-	for {
-		n, err := reader.Read(buffer)
-		if err != nil && err != io.EOF {
-			return fmt.Errorf("oops! error reading response body")
-		}
-
-		if n > 0 {
-			if _, err := out.Write(buffer[:n]); err != nil {
-				return fmt.Errorf("error writing to file:\n%v", err)
-			}
-			downloaded += int64(n)
-		}
-
-		if err == io.EOF {
-			break
-		}
-	}
-
-	// endTime := time.Now()
-	fmt.Printf("\033[32mDownloaded\033[0m [%s]\n", url)
 
 	return nil
 }
